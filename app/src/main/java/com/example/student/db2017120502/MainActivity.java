187c479976f0b1ae67c1d573e844421124f7ce90
Shaojung <shaojung@gmail.com>
2017-12-05 21:29:37 +0800

    use JSONparser

--- app/src/main/java/com/example/student/db2017120502/MainActivity.java
+++ app/src/main/java/com/example/student/db2017120502/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.student.db2017120502;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -16,11 +18,13 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
+    ListView lv;
+    String data[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) findViewById(R.id.textView);
+        lv = (ListView) findViewById(R.id.listView);
         RequestQueue queue = Volley.newRequestQueue(this);
         final StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
                 new Response.Listener<String>() {
@@ -28,10 +32,18 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         try {
                             JSONArray array = new JSONArray(response);
-                            JSONObject obj = array.getJSONObject(0);
-                            String str = obj.getString("district");
-                            String addr = obj.getString("address");
-                            tv.setText(str + "," + addr);
+                            data = new String[array.length()];
+                            for (int i=0;i<array.length();i++)
+                            {
+                                JSONObject obj = array.getJSONObject(i);
+                                String str = obj.getString("district");
+                                String addr = obj.getString("address");
+                                data[i] = str + "," + addr;
+                            }
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                                    android.R.layout.simple_list_item_1, data);
+                            lv.setAdapter(adapter);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
